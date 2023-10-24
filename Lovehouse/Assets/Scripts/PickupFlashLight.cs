b6b2b194a7eb7babec4190a3064e730683f3871f
vhdat <81861683+vhdat@users.noreply.github.com>
2023-10-24 22:37:40 +0700

    Add Flash LIght
    - Add flash light asset
    - Pick up flash light
    - Enable flash light

---
+++ Lovehouse/Assets/Scripts/PickupFlashLight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupFlashLight : MonoBehaviour
+{
+    public GameObject fl_intText, fl_table, fl_hand;
+    public bool fl_interactable;
+    public AudioSource fl_pickup;
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            fl_intText.SetActive(true);
+            fl_interactable = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            fl_intText.SetActive(false);
+            fl_interactable = false;
+        }
+    }
+    void Update()
+    {
+        if (fl_interactable == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                fl_intText.SetActive(false);
+                fl_interactable = false;
+                // fl_pickup.Play();
+                fl_hand.SetActive(true);
+                fl_table.SetActive(false);
+            }
+        }
+    }
+}
